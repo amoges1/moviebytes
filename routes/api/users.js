8f9f0e87bed9b2daa42f80a11a1ff4abfd0f006d
Anteneh M <amoges1@gmail.com>
2019-12-24 16:24:49 -0500

    updated users, setup server side validation check

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
-// @route   GET api/users
-// @desc    Test route
+const {
+    check,
+    validationResult
+} = require('express-validator/check')
+// @route   POST api/users
+// @desc    User registration
 // @access  Public
-router.get('/', (req, res) => res.send('User'))
+router.post('/', [
+    //based on express validator check doc
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({
+        min: 6
+    })
+], (req, res) => {
+    const errors = validationResult(req) //compare from above
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        }) // bad request
+    }
+    console.log(req.body.name);
+    res.send(req.body.name)
+})
 
 module.exports = router;
