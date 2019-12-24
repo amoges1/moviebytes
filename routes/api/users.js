e9a526265122990418b7bf6cee3c842872db7227
Anteneh M <amoges1@gmail.com>
2019-12-24 17:43:06 -0500

    updated file locations, added user registrations

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const {
     check,
     validationResult
-} = require('express-validator/check')
+} = require('express-validator')
+
+const User = require('../../models/User')
 // @route   POST api/users
 // @desc    User registration
 // @access  Public
@@ -14,15 +17,44 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters').isLength({
         min: 6
     })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req) //compare from above
     if (!errors.isEmpty()) {
         return res.status(400).json({
             errors: errors.array()
         }) // bad request
     }
-    console.log(req.body.name);
-    res.send(req.body.name)
+
+    const {
+        name,
+        email,
+        password
+    } = req.body
+
+    try {
+
+        // Find user
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ errors: [{msg: 'User already exists' }]})
+        }
+
+        //create User
+        user = new User({
+            name, email, password
+        })
+
+        const salt = await bcrypt.genSalt(10); //recommended 10, higher better; used for hashing
+        user.password = await bcrypt.hash(password, salt); 
+
+        await user.save() //use await instead of promise.then
+
+        res.send('User registered')
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
 })
 
 module.exports = router;
