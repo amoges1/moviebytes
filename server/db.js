c69738ff5d29744af123e2a3b3717d73877ee7ad
amoges1 <am4970@nyu.edu>
2020-09-20 21:50:34 -0400

    prep for heroku deploy

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const config = require('config');
 const uri = config.get('mongoURI')
 const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(uri, { 
+const client = new MongoClient(process.env.MONGODB_URI || uri, { 
     useNewUrlParser: true,
     useUnifiedTopology: true     
 });
